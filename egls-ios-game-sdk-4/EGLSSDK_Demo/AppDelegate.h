d5d5b41a725e35f2321e39109e8282dc728d6afb
EGLS <sonicdj1987@gmail.com>
2018-07-14 12:08:50 +0800

    005

--- egls-ios-game-sdk-4/EGLSSDK_Demo/AppDelegate.h
+++
@@ -1,8 +0,0 @@
-
-#import <EGLSSDK_Framework/EGLSSDK.h>
-
-@interface AppDelegate : UIResponder <UIApplicationDelegate, EGLSSDKDelegate>
-
-@property (strong, nonatomic) UIWindow * window;
-@property (strong, nonatomic) UIViewController * vc000;
-@end
